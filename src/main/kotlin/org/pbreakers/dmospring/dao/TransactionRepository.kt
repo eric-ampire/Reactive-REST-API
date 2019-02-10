1574a6d69119a5b72b310882388ea7318ab28047
Eric Ampire <ericampire.top@gmail.com>
2019-02-10 22:15:21 +0200

    Recuperation des transtions d'une societe

--- src/main/kotlin/org/pbreakers/dmospring/dao/TransactionRepository.kt
+++ src/main/kotlin/org/pbreakers/dmospring/dao/TransactionRepository.kt
@@ -1,6 +1,10 @@
 package org.pbreakers.dmospring.dao
 
+import org.pbreakers.dmospring.entities.Societe
 import org.pbreakers.dmospring.entities.Transaction
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
+import reactor.core.publisher.Flux
 
-interface TransactionRepository : ReactiveMongoRepository<Transaction, String>
+interface TransactionRepository : ReactiveMongoRepository<Transaction, String> {
+    fun findBySociete(societe: Societe): Flux<Transaction>
+}
